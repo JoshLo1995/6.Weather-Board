6854f06f59a8fa41784b85f6e64e2d717d443f9a
Joshua Lo <josh.lol1995@gmail.com>
2020-04-15 11:09:40 -0700

    did most of the assignment

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# 6.Weather-Board
+# Weather Dashboard
+This is the sixth assignment for the UW Coding Bootcamp Winter 2019. 
+
+## Getting Started
+Download the repository and begin by opening the index.html file.
+
+## Link
+https://joshlo1995.github.io/5.Day-Planner/
+
+## Screenshot
+![Screenshot](assets/images/Screenshot.png)]
+
+## Authors
+- Joshua Lo
+
+## Acknowledgements
+- My dogs, Taco and Nando
+- John Cena
--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 var prevSearched = [];
 var searchURL = "http://api.openweathermap.org/data/2.5/forecast?q=";
 const apiKey = "&APPID=7e6a24a68dbf04f8280d98667782c96b"
-var weatherData;
-$(document).ready(() => {
+//var weatherData;
+var date;
+var iconImage;
+var temperature;
+var humidity;
+var windSpeed;
+var uvIndex;
+const storeage = window.localStorage;
 
+// Set to 0 for the first time that the program runs
+// var counter = 0;
+var prevSearchedCities = [];
+
+$(document).ready(() => {
+    var counter;
+    counter = loadPrevSearched(counter);
 
     // Submit button click listener
     $("#submitButton").click(() => {
         console.log("Submit has been clicked!");
 
-        let returnData = searchCity(weatherData);
-        console.log(returnData);
-        addPrevSearched();
-        getWeatherData(returnData);
+        getWeatherData();
+        addPrevSearched(counter);
+        clearSearchField();
     });
 
     // // Search bar action on 'enter' key
@@ -31,48 +43,104 @@ $(document).ready(() => {
 
 // Load list of prevSearched cities from localStorage
     // append to #prevSearched
-function loadPrevSearched() {
+function loadPrevSearched(counter) {
+    if (localStorage.getItem('counter') == null) {
+        counter = 0;
+        console.log('Running null code');
+        return counter;
+    } else {
+        console.log("Not running null code");
+        counter = localStorage.getItem('counter');
+    
+        for (let i = 0; i <= counter; i++) {
+            prevCity = localStorage.getItem(`searchedCity${i}`);
+            prevSearchedCities.push(prevCity);
+            $("#prevSearched").append(`<li>${prevCity}</li>`);
+        }
+    }
 
+    return counter;
 }
 
 // Search city and get weather data
-function searchCity() {
+function getWeatherData() {
     let searchCity = $("#searchBar").val();
-    let data;
 
     // pass searchCity into an api call
     $.ajax({
         url: searchURL + searchCity + apiKey,
         success: (result) => {
-            data = result;
+            let data = result.list;
+            console.log(data);
+            
+            for (let i = 0; i < 6; i++) {
+                // Assign variables to results and then call display weather
+                date = data[i].dt_txt;
+                iconImage = data[i].weather[0].icon;
+                temperature = `${Math.floor((data[i].main.temp - 273.15) * (9/5) + 32)}F`; // Freedom unit conversion
+                humidity = `${data[i].main.humidity}%`;
+                windSpeed = `${data[i].wind.speed} miles/hr`;
+    
+                $("#weatherDisplay" + i).empty();
 
-            return data;
+                displayWeatherData(date, iconImage, temperature, humidity, windSpeed, i, searchCity);
+            }
         }
     });
-
-    return data;
 }
 
 // add to prevSearched
-function addPrevSearched() {
+function addPrevSearched(counter) {
+    let searched = false;
     let addToSearched = $("#searchBar").val();
 
-    // append to #prevSearched 
-    $("#prevSearched").append(`<li>${addToSearched}</li>`);
+    // Check to see if addToSearched is in previouslySearched
+    if (addToSearched in prevSearchedCities) {
+        // if addtosearched is in prevsearched, don't add
+        searched = true;
 
-    // save prevSearched to localstorage
+    }
+    if (!searched) {
+        // append to #prevSearched 
+        $("#prevSearched").append(`<li>${addToSearched}</li>`);
+
+        // save prevSearched to localstorage
+        counter++;
+        localStorage.setItem(`searchedCity${counter}`, addToSearched);
+        localStorage.setItem('counter', counter);
+        console.log(counter);
+        return counter;
+    }   
 }
+
 // Get weather data
-function getWeatherData(data) {
-    let date;
-    let iconImage;
-    let temperature;
-    let humidity;
-    let windSpeed;
-    let uvIndex;
-
-}
 // Display weather data
-function displayWeatherData() {
+function displayWeatherData(date, iconImage, temperature, humidity, windSpeed, i, searchCity) {
+    iconURL = `http://openweathermap.org/img/w/${iconImage}.png`;
+    // Load today's weather in main block
+    // Then load weather into the rest of the blocks
 
+    if (i == 0) {
+        $("#weatherDisplay" + i).append(`${searchCity} ${moment().format('MMMM Do')}`);
+        appendGeneric();
+    } else {
+        $("#weatherDisplay" + i).append(`<div>${moment().format('MMMM Do')}</div>`);
+        appendGeneric();
+    }
+
+    if (i == 0) {
+        $("#weatherDisplay" + i).append(`<div>Wind Speed: ${windSpeed}</div>`);
+    } 
+
+    function appendGeneric() {
+
+        $("#weatherDisplay" + i).append(`<img src = ${iconURL}>`);
+        $("#weatherDisplay" + i).append(`<div>Temp: ${temperature}</div>`);
+        $("#weatherDisplay" + i).append(`<div>Humidity: ${humidity}</div>`);
+    }
+    
+}
+
+function clearSearchField() {
+    
 }
